b393382fbb8f1a71a5afc16b7f4a4c010251edaf
rodlferreira2 <rodriggo.lferreira@icloud.com>
2022-10-23 21:46:26 -0300

    start do chamado à API

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,60 @@
+import { PrismaClient } from '@prisma/client';
 import express from 'express'
 
-const app = express()
 
-app.get('/games', (request, response) => {
-    return response.json([])
+const app = express()
+const prisma = new PrismaClient({
+    log: ['query']
+})
+
+app.get('/games', async (request, response) => {
+
+    const games = await prisma.game.findMany({
+        include: {
+            _count: {
+                select: {
+                    ads: true,
+                }
+            }
+        }
+    })
+
+    return response.json(games)
 });
 
 app.post('/ads', (request, response) => {
     return response.status(201).json([])
 });
+
+app.get('/games/:id/ads', async (request, response) => {
+    const gameId = request.params.id;
+
+    const ads = await prisma.ad.findMany({
+        select: {
+            id: true,
+            name: true,
+            weekDays: true,
+            useVoiceChannel: true,
+            yearsPlaying: true,
+            hourStart: true,
+        },
+
+        where: {
+            gameId,
+        },
+
+        orderBy: {
+            createdAt: 'desc'
+        }
+    })
+
+    return response.json(ads.map(ad => {
+        return {
+            ...ad,
+            weekDays: ad.weekDays.split(',')
+        }
+    }))
+})
 
 app.get('/games/:id/ discord', (request, response) => { 
     //const adId = request.params.id;
